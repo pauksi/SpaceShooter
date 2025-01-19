44d482a1b264cfa7ccc2137377203e97e5887401
Htet Oo Wai <mghtetoowai@gmail.com>
2025-01-19 10:31:44 +0630

    basic stuffs and creating window

--- code/main.py
+++ code/main.py
@@ -2,4 +2,20 @@ import pygame
 
 # General setup
 pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Space Shooter")
+running = True
 
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #draw the game
+    display_surface.fill('blue')
+    pygame.display.update()
+
+pygame.quit()
